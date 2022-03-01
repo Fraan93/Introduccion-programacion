28ac095e0aea328a3e049ef6c34475db92a5da7c
Fran <fhidalgoj93@gmail.com>
2022-03-01 20:57:21 +0100

    Ejercicio Switch

---
+++ Switch/Switch.java
@@ -0,0 +1,27 @@
+public class Switch {
+    public static void main(String[] args) {
+        String estacion = "a";
+        switch (estacion) {
+            case "Invierno":
+                System.out.println("Es invierno");
+                break;
+            case "Otoño":
+                System.out.println("Es Otoño");
+                break;
+            case "Primavera":
+                System.out.println("Es Primavera");
+            case "Verano":
+                System.out.println("Es Verano");
+                break;
+            default:
+                System.out.println("Es otra estación...");
+
+        }
+    }
+}
+
+
+
+
+
+
