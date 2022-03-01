74e5a4d6aa15eb58f0d87a74ff2674bcf06b0331
Fran <fhidalgoj93@gmail.com>
2022-03-01 20:13:01 +0100

    Subido Ejercicio 2

---
+++ EjercicioWhile/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int numeroWhile = 0;
+
+        while (numeroWhile < 3) {
+            numeroWhile++;
+            System.out.println(numeroWhile);
+        }
+
+
+    }
+}
---
+++ EjerciciosControl1/src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int numerozero;
+        numerozero = -15;
+
+        if (numerozero < 0) {
+            System.out.println("Es negativo");
+        } else if (numerozero > 0){
+            System.out.println("Es positivo");
+        } else {
+            System.out.println("Es 0");
+        }
+    }
+}
